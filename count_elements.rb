699927b412c41a98046593d42019b8f89b79cf2a
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-03-23 19:03:10 -0400

    counting elements

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
 def count_elements(array)
-  # code goes here
+  count_hash = {}
+  array.each do |element|
+    if count_hash[element] != nil
+      count_hash[element] +=1
+    else
+      count_hash[element] = 1
+    end
+  end
+  count_hash
 end
- 
